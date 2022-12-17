1e408ffaf19737d59e4b54f403320fee56f1595a
itt828 <iittt828@gmail.com>
2022-12-17 23:48:42 +0900

    :fire: remove ws.ts

--- src/api/ws.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from '.'
-
-export const ws = (client: Client) => async (playerId: string) => {
-  return await fetch(`${client.baseUrl}/ws?playerId=${playerId}`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }).then((res) => res.text())
-}
