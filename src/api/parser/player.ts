41440ecf84413b53ea060d7e46705092bfce02c1
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2022-12-20 16:00:34 +0900

    ♻️ z infer 使うように

--- src/api/parser/player.ts
+++ src/api/parser/player.ts
@@ -1,13 +1,8 @@
 import { z } from 'zod'
 
-export interface Player {
-  id: string
-  name: string
-  life: number
-}
-
 export const playerSchema = z.object({
   id: z.string(),
   name: z.string(),
   life: z.number(),
 })
+export type Player = z.infer<typeof playerSchema>
--- src/api/parser/room.ts
+++ src/api/parser/room.ts
@@ -1,24 +1,15 @@
 import { z } from 'zod'
-import { Player, playerSchema } from './player'
-
-export interface createRoom {
-  id: string
-  players: Player[]
-  startedAt: string
-}
-
-export interface getRoom {
-  id: string
-  player: Player[]
-}
+import { playerSchema } from './player'
 
 export const roomSchema = z.object({
   id: z.string(),
   players: z.array(playerSchema),
   startedAt: z.string(),
 })
+export type Room = z.infer<typeof roomSchema>
 
 export const getRoomSchema = z.object({
   id: z.string(),
   players: z.array(playerSchema),
 })
+export type GetRoom = z.infer<typeof getRoomSchema>
